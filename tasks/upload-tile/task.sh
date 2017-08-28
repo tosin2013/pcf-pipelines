0f4d71c7455a684cd140911aa84d432e0dc0ff0b
s-matyukevich <s.matyukevich@gmail.com>
2017-08-28 16:02:06 -0600

    pivnet resource name fixed

--- tasks/upload-tile/task.sh
+++ tasks/upload-tile/task.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -e
 
-source ./pcf-pipelines/functions/set-opsman-env.sh
-
 FILE_PATH=`find ./pivnet-product -name *.pivotal`
 om-linux -t $OPSMAN_URL -u $OPSMAN_USER -p $OPSMAN_PASSWORD -k --request-timeout 3600 upload-product -p $FILE_PATH
