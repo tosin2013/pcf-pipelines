175836f0ceecdb5b6502a2b0bd89619fd8c511cc
cxhercules <cxhercules@users.noreply.github.com>
2017-08-28 15:45:14 -0700

    Fixes for redis network config

--- tasks/config-tile/task.sh
+++ tasks/config-tile/task.sh
@@ -20,7 +20,7 @@ tile_network=$(
 om-linux \
   --target https://$OPS_MGR_HOST \
   --username $OPS_MGR_USR \
-  --password $OPS_MGR_PWD \  
+  --password $OPS_MGR_PWD \
   --skip-ssl-validation \
   configure-product \
   --product-name $PRODUCT_NAME \
