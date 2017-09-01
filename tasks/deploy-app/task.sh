eaa41dce2b520668b89c4211e42aad076b86ba68
s-matyukevich <s.matyukevich@gmail.com>
2017-09-01 16:46:18 -0600

    manifest path updated

--- tasks/deploy-app/task.sh
+++ tasks/deploy-app/task.sh
@@ -18,7 +18,7 @@ export pcf_admin_username="admin"
 export pcf_admin_password=$(cat pcf-manifest.json | jq -r '.instance_groups[] | select(.name=="uaa").properties.uaa.scim.users[] | select(.name=="admin").password')
 
 cd app
-erb < ./pcf-pipelines/manifests/$APP_NAME.yml.erb > manifest.yml
+erb < ../pcf-pipelines/manifests/$APP_NAME.yml.erb > manifest.yml
 cf auth $pcf_admin_username $pcf_admin_password
 cf target -o $ORG -s $SPACE
 cf push
