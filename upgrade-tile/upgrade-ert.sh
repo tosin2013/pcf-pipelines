aae47e64d36d2b49a9e846cdd8d77d6dea11bfce
s-matyukevich <s.matyukevich@gmail.com>
2017-09-06 19:51:41 -0600

    upgrade ert install script changed

--- upgrade-tile/upgrade-ert.sh
+++ upgrade-tile/upgrade-ert.sh
@@ -10,6 +10,6 @@ else
 fi
 
 yaml-patch -o ../operations/use-different-git-repo.yml <pipeline.yml > tmp-pipeline.yml
+sed -i -e "s/{{/((/g" -e "s/}}/))/g" tmp-pipeline.yml 
 
-$FLYCMD -t $CONCOURSE_TARGET set-pipeline -p upgrade-elastic-runtime -c tmp-pipeline.yml \
-   -l upgrade-ert-vars-sd.yml $nonint
+$FLYCMD -t $CONCOURSE_TARGET set-pipeline -p upgrade-elastic-runtime -c tmp-pipeline.yml  $nonint
