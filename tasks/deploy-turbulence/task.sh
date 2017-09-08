eefa168e8bf6e500e59b5e6969b367865adcdf24
s-matyukevich <s.matyukevich@gmail.com>
2017-09-08 11:47:59 -0600

    env vars names fixed

--- tasks/deploy-turbulence/task.sh
+++ tasks/deploy-turbulence/task.sh
@@ -4,8 +4,8 @@ source ./pcf-pipelines/functions/export_bosh_credentials.sh
 
 bosh -n -d turbulence deploy ./pcf-pipelines/tasks/deploy-turbulence/turbulence.yml \
   -v turbulence_api_ip=$TURBULENCE_API_IP \
-  -v director_ip=$BOSH_IP \
-  --var-file director_ssl_ca=$DIRECTOR_CA_CERT \
+  -v director_ip=$BOSH_ENVIRONMENT \
+  --var-file director_ssl_ca=$BOSH_CA_CERT \
   -v director_client=$BOSH_CLIENT \
   -v director_client_secret=$BOSH_CLIENT_SECRET 
 
