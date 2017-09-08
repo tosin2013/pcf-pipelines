5b1f8dbc841a2999b105c9979d9942a2fe89a40a
s-matyukevich <s.matyukevich@gmail.com>
2017-09-08 11:50:22 -0600

    var defenition changed

--- tasks/deploy-turbulence/task.sh
+++ tasks/deploy-turbulence/task.sh
@@ -5,7 +5,7 @@ source ./pcf-pipelines/functions/export_bosh_credentials.sh
 bosh -n -d turbulence deploy ./pcf-pipelines/tasks/deploy-turbulence/turbulence.yml \
   -v turbulence_api_ip=$TURBULENCE_API_IP \
   -v director_ip=$BOSH_ENVIRONMENT \
-  --var-file director_ssl_ca=$BOSH_CA_CERT \
+  -v director_ssl_ca=$BOSH_CA_CERT \
   -v director_client=$BOSH_CLIENT \
   -v director_client_secret=$BOSH_CLIENT_SECRET 
 
