17a7d2b50cf5ae4be016b79365a5ca62038f9be4
s-matyukevich <s.matyukevich@gmail.com>
2017-09-11 14:41:23 -0600

    var file fixed

--- tasks/deploy-turbulence/task.sh
+++ tasks/deploy-turbulence/task.sh
@@ -7,7 +7,7 @@ echo "$BOSH_CA_CERT" > bosh.pem
 bosh -n -d turbulence deploy ./pcf-pipelines/tasks/deploy-turbulence/turbulence.yml \
   -v turbulence_api_ip=$TURBULENCE_API_IP \
   -v director_ip=$BOSH_ENVIRONMENT \
-  --vars-file director_ssl_ca=bosh.pem \
+  --var-file director_ssl_ca="./bosh.pem" \
   -v director_client=$BOSH_CLIENT \
   -v director_client_secret=$BOSH_CLIENT_SECRET 
 
