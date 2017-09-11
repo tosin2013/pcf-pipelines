6d6f4d30da8413576df0163e15da34859b469003
s-matyukevich <s.matyukevich@gmail.com>
2017-09-11 14:36:30 -0600

    bosh ca written on disk

--- tasks/deploy-turbulence/task.sh
+++ tasks/deploy-turbulence/task.sh
@@ -2,10 +2,12 @@
 
 source ./pcf-pipelines/functions/export_bosh_credentials.sh
 
+echo "$BOSH_CA_CERT" > bosh.pem
+
 bosh -n -d turbulence deploy ./pcf-pipelines/tasks/deploy-turbulence/turbulence.yml \
   -v turbulence_api_ip=$TURBULENCE_API_IP \
   -v director_ip=$BOSH_ENVIRONMENT \
-  -v director_ssl_ca="$BOSH_CA_CERT" \
+  --vars-file director_ssl_ca=bosh.pem \
   -v director_client=$BOSH_CLIENT \
   -v director_client_secret=$BOSH_CLIENT_SECRET 
 
