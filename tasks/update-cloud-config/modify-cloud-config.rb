ec3bdf9172f85aca6846a5a639a2dd23f1881fdf
s-matyukevich <s.matyukevich@gmail.com>
2017-09-11 12:53:05 -0600

    update cc task fixed

--- tasks/update-cloud-config/modify-cloud-config.rb
+++ tasks/update-cloud-config/modify-cloud-config.rb
@@ -2,10 +2,11 @@
 
 require 'yaml'
 
-cc = YAML.load(string)
-static = cc['networks'].find {|x| x["name"] == "services"}["static"]
-if static.any? then
+cc = YAML.load(STDIN.read)
+static = cc['networks'].find {|x| x["name"] == "SERVICES"}['subnets'][0]["static"]
+if !static.any? then
   static.push(ENV['SERVICES_NETWORK_STATIC_IPS'])
 end
 
 puts YAML.dump(cc)
+
