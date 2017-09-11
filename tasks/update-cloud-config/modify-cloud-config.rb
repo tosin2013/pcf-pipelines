d829248513376d6b17e19ff377cf47eba35f77f1
s-matyukevich <s.matyukevich@gmail.com>
2017-09-11 13:38:15 -0600

    cc static addr defenition updated

--- tasks/update-cloud-config/modify-cloud-config.rb
+++ tasks/update-cloud-config/modify-cloud-config.rb
@@ -3,7 +3,10 @@
 require 'yaml'
 
 cc = YAML.load(STDIN.read)
-static = cc['networks'].find {|x| x["name"] == "DYNAMIC-SERVICES"}['subnets'][0]["static"]
+static = cc['networks'].find {|x| x["name"] == "DYNAMIC-SERVICES"}['subnets'][0]['static']
+if static.nil? then 
+  static = cc['networks'].find {|x| x["name"] == "DYNAMIC-SERVICES"}['subnets'][0]['static'] = []
+end
 if !static.any? then
   static.push(ENV['DYNAMIC_SERVICES_NETWORK_STATIC_IPS'])
 end
