ca73d6d370549f36507a56fd76edd1b64ccf9713
s-matyukevich <s.matyukevich@gmail.com>
2017-09-12 10:25:22 -0600

    common script location changed

--- tasks/chaos-loris/deploy.sh
+++ tasks/chaos-loris/deploy.sh
@@ -3,9 +3,8 @@ set -e
 
 ### Load env
 
-project_dir=$(readlink -f "$(dirname $0)/../..")
-source $project_dir/common/utils/load-cf-env.sh
-source $project_dir/common/utils/cf-helpers.sh
+source ./pcf-pipelines/functions/export_cf_credentials.sh
+source ./pcf-pipelines/functions/cf-helpers.sh
 
 cf_authenticate_and_target
 cf_target_org_and_space system chaos-loris
