8996d0f6233e55e89f102c4f39b1f9e4d1fc8b3a
s-matyukevich <s.matyukevich@gmail.com>
2017-09-14 18:18:47 -0600

    build cmd fixed

--- tasks/mongo-broker/build.sh
+++ tasks/mongo-broker/build.sh
@@ -3,7 +3,7 @@ set -e
 
 # you need to have java, mvn, ruby, bundler
 pushd broker-repo
-./gradlew -x test  
+./gradlew build -x test  
 popd
 
 mv broker-repo/build/lib/cloudfoundry-mongodb-service-broker.jar binary
