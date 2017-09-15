1859a1f062d7fc89262a66d2b877354415818e46
s-matyukevich <s.matyukevich@gmail.com>
2017-09-14 18:35:18 -0600

    service name changed

--- tasks/mongo-broker/register-service-broker.sh
+++ tasks/mongo-broker/register-service-broker.sh
@@ -8,6 +8,6 @@ source ./pcf-pipelines/functions/cf-helpers.sh
 cf_authenticate_and_target
 cf_target_org_and_space system system
 
-register_broker mongodb-enterprise mongo $MONGO_BROKER_PASSWORD "https://mongodb-service-broker.$APPS_DOMAIN"
+register_broker mongodb mongo $MONGO_BROKER_PASSWORD "https://mongodb-service-broker.$APPS_DOMAIN"
 
-enable_global_access mongodb-enterprise
+enable_global_access mongodb
